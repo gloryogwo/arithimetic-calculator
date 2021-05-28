2ec9e3dde38d401fd5ae8a31d964ad8c2b604a23
glory ogwo <glorieogwo@gmail.com>
2021-05-28 11:38:22 -0700

    add initial paragraph

--- js/script.js
+++ js/script.js
@@ -13,18 +13,39 @@ function multiply(number1, number2) {
 function divide(number1, number2) {
   return number1 / number2;
 }
-//UI logic
-$(document).ready(function(){
+function percentage(number1, number2) {
+  return number1 / number2;
+}
 
-  $("#add").submit(function(event){
+$(document).ready(function() {
+  $("#add").submit(function(event) {
     event.preventDefault();
+    let result=0;
     let firstNumber=$("#add1").val();
     let secondNumber=$("#add2").val();
-
-    let result=add(parseFloat(firstNumber),parseFloat(secondNumber));
-    alert(result);
-
-    
+    const op = $("input:radio[name=Result]:checked").val();
+    if(op==="add")
+    {
+      result=add(parseFloat(firstNumber),parseFloat(secondNumber));
+    }
+    else if(op==="subtract")
+    {
+      result=subtract(parseFloat(firstNumber),parseFloat(secondNumber));
+    }
+    else if(op==="multiply")
+    {
+      result=multiply(parseFloat(firstNumber),parseFloat(secondNumber));
+    }
+    else if(op==="Divide")
+    {
+      result=divide(parseFloat(firstNumber),parseFloat(secondNumber));
+    }
+    else if(op==="percentage")
+    {
+      result=percentage(parseFloat(firstNumber),parseFloat(secondNumber));
+    }
+    $("#output").text(result);
   });
 });
-//$("#story").show();
+ 
+
